e29017b9a4df5f02421b2d0b1f8c2dc4f2317e7a
James Arista Yaputra <jamesyaputra1704@gmail.com>
2020-09-01 21:26:45 +0800

    chore: set up typescript

--- stories/Header.js
+++ stories/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Button } from './Button';
-import './header.css';
+import './header.scss';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
@@ -30,11 +30,11 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         {user ? (
           <Button size="small" onClick={onLogout} label="Log out" />
         ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
+            <>
+              <Button size="small" onClick={onLogin} label="Log in" />
+              <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            </>
+          )}
       </div>
     </div>
   </header>
--- stories/Page.js
+++ stories/Page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Header } from './Header';
-import './page.css';
+import './pages';
 
 export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <article>
