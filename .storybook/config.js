fb05f2321593146c6556a22dbdd93631cf8feee9
James Arista Yaputra <jamesyaputra1704@gmail.com>
2020-12-07 13:50:17 +0800

    chore: moves pages and components to /src

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,7 +5,7 @@ import { withKnobs } from "@storybook/addon-knobs";
 addDecorator(jsxDecorator);
 addDecorator(withKnobs);
 
-const req = require.context("../", true, /\.stories\.tsx$/);
+const req = require.context("../src", true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   purge: [
-    "./pages/**/*.@(js|jsx|ts|tsx)",
-    "./components/**/*.@(js|jsx|ts|tsx)",
+    "./src/pages/**/*.@(js|jsx|ts|tsx)",
+    "./src/components/**/*.@(js|jsx|ts|tsx)",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
