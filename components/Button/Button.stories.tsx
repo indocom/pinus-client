eaf378cec56d42ad617e3178c14591c1e886d1d7
James Arista Yaputra <jamesyaputra1704@gmail.com>
2020-12-07 13:43:12 +0800

    enhancement: button component

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -2,4 +2,6 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import Button from "./index";
 
-storiesOf("Button", module).add("Default", () => <Button>Default</Button>);
+storiesOf("Button", module).add("Base", () => (
+  <Button>This is a button</Button>
+));
