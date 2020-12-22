5521e2ef65fac86dd26a1f66f6f8be90284776ea
Ryo Armanda <ryoarmanda@rocketmail.com>
2020-12-22 21:04:10 +0800

    Modify captioned banner story placeholder

--- src/components/CaptionedBanner/CaptionedBanner.stories.tsx
+++ src/components/CaptionedBanner/CaptionedBanner.stories.tsx
@@ -5,6 +5,6 @@ import CaptionedBanner from "./index";
 storiesOf("CaptionedBanner", module).add("Base", () => (
   <CaptionedBanner
     title="This is a title"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas odio dui, laoreet id consequat finibus, iaculis quis risus. Maecenas quis efficitur eros, a iaculis metus."
+    description="This is a description"
   />
 ));
