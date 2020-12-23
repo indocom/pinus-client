631e054142473495d60792be452b8077d86a8043
James Arista Yaputra <jamesyaputra1704@gmail.com>
2020-12-23 11:07:00 +0800

    fix: addresses PR comments; banner flexbox

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -14,32 +14,38 @@ const Banner: React.FC<OwnProps> = ({
   description,
 }) => {
   return (
-    <div className={`bg-cover bg-no-repeat bg-center bg-${bgImage}`}>
-      <div
-        className={`
+    <div
+      className={`
           flex flex-col
-          font-sans
           justify-center items-center
           w-screen h-screen
           bg-black bg-opacity-50
           lg:p-32
+          font-sans
+      `}
+    >
+      <div
+        className={`
+          bg-cover bg-no-repeat bg-center bg-${bgImage} w-screen
+          flex flex-col flex-grow items-center justify-center
         `}
       >
-        <p className={`text-6xl font-bold text-white text-center`}>{title}</p>
-        {subBanner ? (
-          <div
-            className={`flex flex-row items-center justify-center absolute bottom-0 w-screen bg-gray-200`}
-          >
-            <p className={`text-lg text-center my-12 text-gray-900 max-w-4xl`}>
-              {description}
-            </p>
-          </div>
-        ) : (
+        <p className={`text-6xl font-bold text-white text-center mx-auto`}>
+          {title}
+        </p>
+        {!subBanner && (
           <p className={`text-lg mt-6 text-white text-center max-w-4xl`}>
             {description}
           </p>
         )}
       </div>
+      {subBanner && (
+        <div className={`bg-secondary w-screen py-10`}>
+          <p className={`text-lg text-center max-w-3xl mx-auto`}>
+            {description}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
