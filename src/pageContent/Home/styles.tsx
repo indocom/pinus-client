ba4ae6f186acb84b41984c1ef74069cec3c980fc
James Arista Yaputra <jamesyaputra1704@gmail.com>
2021-01-07 11:28:43 +0800

    enhancement: mobile version of home page

--- src/pageContent/Home/styles.tsx
+++ src/pageContent/Home/styles.tsx
@@ -1,15 +1,16 @@
-export const Section = `grid grid-cols-12 grid-rows-6 gap-6 h-screen`;
+export const Section = `lg:grid-cols-6 lg:gap-4 grid grid-cols-12 grid-rows-6 gap-6 h-screen`;
+export const SectionText = `lg:text-center lg:max-w-md mt-8 mb-14`;
 
-export const ImageBanners = `mt-16 ml-4 grid grid-cols-8 grid-rows-6 gap-6 h-full w-full`;
-export const ImageBanner1 = `col-start-2 col-span-2 row-start-1 row-span-3 bg-home-showcase1 bg-cover bg-top bg-no-repeat shadow-lg`;
-export const ImageBanner2 = `col-start-4 col-span-2 row-start-2 row-span-3 bg-home-showcase2 bg-cover bg-center bg-no-repeat shadow-lg`;
-export const ImageBanner3 = `col-start-6 col-span-2 row-start-3 row-span-3 bg-home-showcase3 bg-cover bg-center bg-no-repeat shadow-lg`;
+export const ImageBanners = `lg:grid-cols-6 lg:ml-2 lg:gap-2 mt-16 ml-4 grid grid-cols-8 grid-rows-6 gap-6 h-full w-full`;
+export const ImageBanner1 = `lg:col-start-1 col-start-2 col-span-2 row-start-1 row-span-3 bg-home-showcase1 bg-cover bg-top bg-no-repeat shadow-lg`;
+export const ImageBanner2 = `lg:col-start-3 col-start-4 col-span-2 row-start-2 row-span-3 bg-home-showcase2 bg-cover bg-center bg-no-repeat shadow-lg`;
+export const ImageBanner3 = `lg:col-start-5 col-start-6 col-span-2 row-start-3 row-span-3 bg-home-showcase3 bg-cover bg-center bg-no-repeat shadow-lg`;
 
-export const RedBanner = `col-start-2 col-span-2 row-start-1 row-span-3 bg-pinus-red`;
-export const BlueBanner = `col-start-4 col-span-2 row-start-1 row-span-4 bg-pinus-blue`;
-export const YellowBanner = `col-start-6 col-span-2 row-start-3 row-span-4 bg-pinus-yellow`;
+export const RedBanner = `lg:col-start-1 col-start-2 col-span-2 row-start-1 row-span-3 bg-pinus-red`;
+export const BlueBanner = `lg:col-start-3 col-start-4 col-span-2 row-start-1 row-span-4 bg-pinus-blue`;
+export const YellowBanner = ` lg:col-start-5 col-start-6 col-span-2 row-start-3 row-span-4 bg-pinus-yellow`;
 
-export const FreshmenHeader = `flex flex-col items-center py-12`;
-export const FreshmenSection = `flex flex-row justify-between px-72 py-28 mb-48 bg-pinus-black text-white`;
+export const FreshmenHeader = `flex flex-col items-center w-full py-12`;
+export const FreshmenSection = `lg:flex-col lg:p-0 lg:py-12 flex flex-row justify-between px-72 py-28 mb-48 bg-pinus-black text-white`;
 
-export const Carousel = `col-start-6 col-span-6 row-start-1 row-span-5`;
+export const Carousel = `lg:px-4 lg:col-span-6 lg:row-span-2 col-start-1 col-span-7 row-start-1 row-span-5`;
