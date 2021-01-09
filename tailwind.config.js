3c4f18b334bcbc7691748080f0b2afd8cc716a8c
James Arista Yaputra <jamesyaputra1704@gmail.com>
2021-01-09 21:58:17 +0800

    fix: removes purge css from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  purge: [
-    "./src/pages/**/*.@(js|jsx|ts|tsx)",
-    "./src/components/**/*.@(js|jsx|ts|tsx)",
-  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     backgroundColor: (theme) => ({
