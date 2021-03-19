b0e461f06b9c5c459fba1c200561ea11b95096cc
simonjulianl <simonjulian21@gmail.com>
2021-03-19 15:09:05 +0800

    bug : fix aksarabox story error

--- src/components/AksaraBox/AksaraBox.stories.tsx
+++ src/components/AksaraBox/AksaraBox.stories.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import AksaraBox from "./index";
 
-storiesOf("AksaraBox", module)
-  .add("Base", () => (
-    <AksaraBox title="This is a title" description="This is a description" />
-  ))
-  
+storiesOf("AksaraBox", module).add("Base", () => (
+	<AksaraBox
+		title="This is a title"
+		description="This is a description"
+		hyperlink="https://google.com"
+	/>
+));
