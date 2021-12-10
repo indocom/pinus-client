1c8bfab14dedf499438e4fc443885401a18ac26c
bernarduskrishna <77195969+bernarduskrishna@users.noreply.github.com>
2021-12-10 23:01:03 +0800

    Delete unused import

--- src/pageContent/Home/index.tsx
+++ src/pageContent/Home/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-// import Carousel from "src/components/Carousel";
-import { Text, Button, Carousel } from "pinus-ui-library";
+import { Text, Button } from "pinus-ui-library";
 import * as S from "./styles";
 
 const HomeContent: React.FC = () => {
