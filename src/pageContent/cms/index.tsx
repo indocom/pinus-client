7de3621fc583fb09b452c93305e52acbbfba5a59
Amadeus <amadeusw6@gmail.com>
2021-12-28 13:27:33 +0800

    Add contentful support for admissions
    Admission supports Contentful to a large extent now.

--- src/pageContent/cms/index.tsx
+++ src/pageContent/cms/index.tsx
@@ -1,45 +1,93 @@
-import { createClient } from "contentful";
+import { createClient, Entry } from "contentful";
 import React from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { Document } from '@contentful/rich-text-types';
+import { getAllDocsFromCMS } from "src/lib/ssg";
 
-async function getItems() {
+// interface DocMeta {
+//   title: string,
+//   section: string,
+//   chapter: string, 
+//   subchapter: string, 
+//   post: Document, 
+//   slug: string
+// }
+
+interface DocMeta {
+  title: string;
+  chapter: string;
+  subchapter: string;
+  section: string;
+  content?: string;
+  slug?: string | string[];
+  post?: Document
+}
+
+interface slugRetrieved {
+  slug: string
+}
+
+async function getDocSlugs(): Promise<string[]> {
   const client = createClient({
     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
   });
 
-  const res = await client.getEntries({ content_type: "trial1" });
+  const res = await client.getEntries<slugRetrieved>({
+    content_type: "admissions", 
+    select: "fields.slug"
+  })
 
-  return res.items;
-}
+  return res.items.map((x: Entry<slugRetrieved>) => x.fields.slug);
+};
 
-interface Entry {
-  // eslint-disable-next-line
-  [key: string]: any;
+async function getDocBySlug(slug: string): Promise<DocMeta> {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries<DocMeta>({
+    content_type: "admissions", 
+    'fields.slug[match]': slug
+  })
+
+  // Obviously wrong + potentially buggy implementation since match may return more than one doc
+  // To be dealt with some other time
+  return res.items[0].fields;
 }
 
 const Cms: React.FC = () => {
   React.useEffect(() => {
     async function getData() {
-      const res = await getItems();
-      setData(res);
+      const res = await getDocSlugs();
+      let docs = await Promise.all(res.map(async slug => await getDocBySlug(slug)));
+
+      docs = await getAllDocsFromCMS();
+
+      setData(docs);
     }
     getData();
   }, []);
 
-  const [data, setData] = React.useState<Array<Entry>>();
-  if (!data) {
-    return <div></div>;
+  const [posts, setData] = React.useState<Array<DocMeta>>();
+  if (!posts) {
+    return <div>No posts found!</div>;
   }
-  console.log(data);
+  console.log(posts);
   return (
     <div>
-      {data.map((post: Entry) => (
-        <div>
-          <div key={post.sys.id}>{post.fields.title}</div>
-          <div> {documentToReactComponents(post.fields.trialRichText)}</div>
-        </div>
-      ))}
+      {posts.map((post: DocMeta, index) => 
+        (
+          <div>
+            <div key={index}>{post.slug}</div>
+            <div key={index}>{post.section}</div>
+            <div key={index}>{post.chapter}</div>
+            <div key={index}>{post.subchapter}</div>
+            <div> {documentToReactComponents(post.post)}</div>
+          </div>
+        )
+      )}
     </div>
   );
 };
