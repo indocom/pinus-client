733d90d130c3e824eeff05aac95b9a4e0f4ddb64
Winston Cahya <winstoncahya90@gmail.com>
2022-01-04 23:12:00 +0800

    Sections' deletion is restored

--- src/pageContent/Events/util.tsx
+++ src/pageContent/Events/util.tsx
@@ -1,10 +1,34 @@
-import { sections, EventData } from "./data";
+import {EventData, getItems} from "./data";
+import React from "react";
+
 
 export const getEventById = (id: string): EventData | undefined => {
-  const s = sections.find((e) => e.id === id);
+  React.useEffect(() => {
+    async function getData() {
+      const res = await getItems();
+      setData(res);
+    }
+    getData();
+  }, []);
+  interface Entry {
+    // eslint-disable-next-line
+    [key: string]: any;
+  }
+  const [data, setData] = React.useState<Array<Entry>>();
+  
+  const eventsData:Array<EventData> = data.map(data => {
+    return {
+    name: data.fields.name,
+    description: data.fields.eventDescription,
+    url: data.fields.url,
+    imageSrc: "http:" + data.fields.eventPicture.fields.file.url}
+    
+  });
+  const searchedUrl = "/events/" + id
+  const s = eventsData.find((e) => e.url === searchedUrl);
   if (s === undefined) {
     return undefined;
   }
 
-  return s.data;
+  return s;
 };
