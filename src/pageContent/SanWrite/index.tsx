6b4811279185cf0a1856e4258a0dfb44569fe218
bernarduskrishna <77195969+bernarduskrishna@users.noreply.github.com>
2022-01-31 14:10:38 +0800

    Content can be published now, but not linked to a recipient.

--- src/pageContent/SanWrite/index.tsx
+++ src/pageContent/SanWrite/index.tsx
@@ -1,13 +1,76 @@
 import React, { useState } from "react";
 
-import { getPersons } from "src/utils/contentful/kudo";
+import { getPersons, createContent } from "src/utils/contentful/kudo";
 
-import { Text, Button, Input, TextArea } from "pinus-ui-library";
+import { Dropdown, Text, Button, Input, TextArea } from "pinus-ui-library";
 
 const SanWriteContent: React.FC = () => {
+  const [persons, setPersons] = useState<Array<string>>();
+  const [recipient, setRecipient] = useState<string>("");
+  const [writer, setWriter] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  React.useEffect(() => {
+    async function getData() {
+      const res = await getPersons();
+
+      setPersons(res);
+    }
+    getData();
+  }, []);
+
+  type entry = {
+    label: string;
+    value: string;
+  };
+
+  async function handleSubmit() {
+    if (recipient && writer && content) {
+      await createContent(content, recipient, writer);
+      setMessage("Message successfully posted");
+    } else {
+      setMessage("All fields must be filled in");
+    }
+  }
+
   return (
-    <div>
-      <div>asdfasdfa</div>
+    <div className={`flex flex-col items-center w-95vw`}>
+      <p>&nbsp;</p>
+      <Text fontSize="xl"> Recipient </Text>
+      <Dropdown
+        onChange={(option: entry) => {
+          option && setRecipient(option.label);
+        }}
+        isMulti={false}
+        options={
+          persons &&
+          persons.map((entry) => {
+            return {
+              label: entry,
+              value: entry,
+            };
+          })
+        }
+        placeholder="Recipient"
+      />
+      <p>&nbsp;</p>
+      <Text fontSize="xl"> Your message </Text>
+      <TextArea
+        value={content || ""}
+        placeholder="Your message"
+        onChange={(event) => setContent(event.target.value)}
+      />
+      <p>&nbsp;</p>
+      <Text fontSize="xl"> Your name </Text>
+      <Input
+        value={writer || ""}
+        placeholder="Your name"
+        onChange={(event) => setWriter(event.target.value)}
+      />
+      <p>&nbsp;</p>
+      <Button onClick={handleSubmit} label="Submit" variant="secondary" />
+      <Text color="red">{message}</Text>
     </div>
   );
 };
--- src/utils/contentful/kudo.tsx
+++ src/utils/contentful/kudo.tsx
@@ -4,6 +4,39 @@ const generateRandomString = (length = 6) => {
   return Math.random().toString(20).substring(2, length);
 };
 
+export async function createContent(
+  content: string,
+  recipientName: string,
+  writerName: string
+): Promise<Entry> {
+  const client = createClient({
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
+  });
+
+  // TODO: A CONTENT IS PUBLISHED BUT NOT LINKED TO THE RECIPIENT. NOT SURE HOW TO DO THAT
+  return client
+    .getSpace(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID)
+    .then((space) => space.getEnvironment("master"))
+    .then((env) => {
+      const entryId = generateRandomString(10);
+      return env.createEntryWithId(
+        "content", // content type ID. Check contentful on the type's ID
+        entryId,
+        {
+          fields: {
+            text: {
+              "en-US": content,
+            },
+            writer: {
+              "en-US": writerName,
+            },
+          },
+        }
+      );
+    })
+    .then((entry) => entry.publish());
+}
+
 export async function createPerson(personName: string): Promise<Entry> {
   const client = createClient({
     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
