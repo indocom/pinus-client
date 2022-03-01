a1d35070aecf2d8f42ca3cd9e5c2d0f1751b19c1
Winston Cahya <winstoncahya90@gmail.com>
2022-03-01 20:28:44 +0800

    Revert "Salah Merge"
    This reverts commit 2774665ff96be76e7a658356938557d0424b27d8, reversing
    changes made to 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f.

--- src/pageContent/SanWrite/index.tsx
+++ src/pageContent/SanWrite/index.tsx
@@ -1,42 +1,5 @@
 import React, { useState } from "react";
 
-<<<<<<< HEAD
-import { getPersons, createAndLink } from "src/utils/contentful/kudo";
-
-import { Dropdown, Text, Button, Input, TextArea } from "pinus-ui-library";
-
-const SanWriteContent: React.FC = () => {
-  const [persons, setPersons] = useState<Array<string>>();
-
-  const [recipient, setRecipient] = useState<string>("");
-  const [writer, setWriter] = useState<string>("");
-  const [content, setContent] = useState<string>("");
-  const [image, setImage] = useState<File>();
-
-  const [message, setMessage] = useState<string>("");
-
-  React.useEffect(() => {
-    async function getData() {
-      const res = await getPersons();
-
-      setPersons(res);
-    }
-    getData();
-  }, []);
-
-  type entry = {
-    label: string;
-    value: string;
-  };
-
-  async function handleSubmit() {
-    if (recipient && writer && content && image) {
-      await createAndLink(writer, recipient, content, image);
-      setMessage("Message successfully posted");
-    } else {
-      setMessage("All fields must be filled in");
-    }
-=======
 import { createContentAndLink } from "src/utils/contentful/kudo";
 
 import { Text, Button, Input, TextArea } from "pinus-ui-library";
@@ -89,34 +52,13 @@ const SanWriteContent = ({ setIsShown, setSubmit, name }) => {
       setMessage("All fields must be filled in");
     }
     setLoading(false);
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
   }
 
   return (
     <div className={`flex flex-col items-center w-95vw`}>
       <p>&nbsp;</p>
-<<<<<<< HEAD
-      <Text fontSize="xl"> Recipient </Text>
-      <Dropdown
-        onChange={(option: entry) => {
-          option && setRecipient(option.label);
-        }}
-        isMulti={false}
-        options={
-          persons &&
-          persons.map((entry) => {
-            return {
-              label: entry,
-              value: entry,
-            };
-          })
-        }
-        placeholder="Recipient"
-      />
-=======
       <Text fontSize="xl"> Write your wishes for {recipient} </Text>
 
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
       <p>&nbsp;</p>
       <Text fontSize="xl"> Your message </Text>
       <TextArea
@@ -133,10 +75,7 @@ const SanWriteContent = ({ setIsShown, setSubmit, name }) => {
       />
       <p>&nbsp;</p>
       <Text fontSize="xl"> Picture (optional) </Text>
-<<<<<<< HEAD
-=======
       <img src={preview} alt={preview} />
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
       <form action="/api/images" method="post">
         <input
           type="file"
@@ -146,10 +85,7 @@ const SanWriteContent = ({ setIsShown, setSubmit, name }) => {
 
             if (files.length !== 1) {
               setImage(null);
-<<<<<<< HEAD
-=======
               setPreview(null);
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
               setMessage(
                 files.length < 1
                   ? "Did you not upload anything??"
@@ -164,21 +100,13 @@ const SanWriteContent = ({ setIsShown, setSubmit, name }) => {
               setMessage("Please upload only pictures");
               return;
             }
-<<<<<<< HEAD
-
-            setImage(file);
-=======
             setMessage("");
             setImage(file);
             previewFile(file);
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
           }}
         />
       </form>
       <p>&nbsp;</p>
-<<<<<<< HEAD
-      <Button onClick={handleSubmit} label="Submit" variant="secondary" />
-=======
       <Button
         onClick={handleSubmit}
         label="Submit"
@@ -187,7 +115,6 @@ const SanWriteContent = ({ setIsShown, setSubmit, name }) => {
         isLoading={isLoading}
         fontSize="xl"
       />
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
       <Text color="red">{message}</Text>
     </div>
   );
--- src/pages/kudos/index.tsx
+++ src/pages/kudos/index.tsx
@@ -1,29 +1,17 @@
 import React from "react";
 import { NextPage } from "next";
 import Page from "src/components/Page";
-<<<<<<< HEAD
-=======
 import { Seniors } from "src/pageContent/Kudos";
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
 
 const Kudos: NextPage = () => {
   return (
     <Page
-<<<<<<< HEAD
-      bgImage=""
-      title="Kudos"
-      subBanner
-      description="Drop your kudos down below !"
-    >
-      Kudos UI to write
-=======
       bgImage="admissions"
       title="Senior Appreciation Night"
       subBanner
       description="Send your well wishes for our graduating batch!"
     >
       <Seniors />
->>>>>>> 7cdc558b9c23f253f406f2d2fb3c10d52231ff0f
     </Page>
   );
 };
