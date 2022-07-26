2ba606fee49b53c2c5c980cacb96619105c996cd
Simon Julian Lauw <simonjulian21@gmail.com>
2022-07-26 22:53:27 +0700

    Ignore eslint

--- next.config.js
+++ next.config.js
@@ -16,4 +16,9 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
